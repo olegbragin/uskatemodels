b87bc77740de785f2c9e95f2d014c8f76e86db47
Oleg Bragin <>
2025-02-24 23:12:21 +0300

    Add new models

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,3 +2,5 @@ export declare const helloWorld: () => string;
 
 export * from '../src/models/user'
 export * from '../src/models/cityroute'
+export * from '../src/models/chargehistoryitem'
+export * from '../src/models/chargestation'
