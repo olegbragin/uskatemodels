b144c6f5b28a20ec5134b5e648ec9a927df73146
Oleg Bragin <>
2025-04-16 12:02:08 +0300

    Add new models

--- src/models/charger.d.ts
+++ src/models/charger.d.ts
@@ -1,11 +1,7 @@
-export interface ChargeStationIdDTO {
-  id?: number
-}
-
 export interface ChargerDTO {
   id?: number
   plug: number
   state: number
   price: string
-  chargeStation?: ChargeStationIdDTO
+  chargeStationId?: number
 }
